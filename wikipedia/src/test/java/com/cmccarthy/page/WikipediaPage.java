e6774e8700a7996d28ceb613e5f5a1fd374ad917
craig <craig.mccarthy@live.ie>
2020-10-02 20:31:37 +0100

    initial commit

---
+++ wikipedia/src/test/java/com/cmccarthy/page/WikipediaPage.java
@@ -0,0 +1,22 @@
+package com.cmccarthy.page;
+
+import com.cmccarthy.config.AbstractPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WikipediaPage extends AbstractPage {
+
+    @FindBy(className = "central-featured-logo")
+    private WebElement centerLogo;
+
+    public void open(String url) {
+        openAt(url);
+    }
+
+    public boolean isPageOpened() {
+        return waitForElementPresent(centerLogo);
+    }
+}
+
